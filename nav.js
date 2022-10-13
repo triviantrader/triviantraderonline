bfe2878482603951ddeafb18062f63a03119eaa3
triviantrader <115366712+triviantrader@users.noreply.github.com>
2022-10-13 12:11:01 +0530

    Add files via upload

--- nav.js
+++ nav.js
@@ -30,7 +30,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    document.getElementById("navbar-secondary").style.height = "50px";
+    document.getElementById("navbar-secondary").style.height = "80px";
     document.getElementById("navbar-secondary").style.boxShadow = "0px 6px 10px rgba(0, 0, 0, 0.3)";
     document.getElementById("navbar-secondary").style.background = "white";
     document.getElementById("navbar-secondary").style.padding = "25px 0px 10px 0px";
@@ -41,20 +41,28 @@ function scrollFunction() {
     document.getElementById("about-link").style.color = "black"
     document.getElementById("contact-link").style.color = "black"
     // document.getElementById("logo").style.fontSize = "25px";
+    console.log(screen.width)
   } else {
-    document.getElementById("navbar-secondary").style.height = "120px";
-    document.getElementById("navbar-secondary").style.boxShadow = "none";
-    document.getElementById("navbar-title").style.fontSize = "28px"
-    document.querySelector(".navbar-title").style.lineHeight = "38px"
-     document.getElementById("home-link").style.color = "white"
-    document.getElementById("service-link").style.color = "white"
-    document.getElementById("about-link").style.color = "white"
-    document.getElementById("contact-link").style.color = "white"
+    if (screen.width > 830) {
+      document.getElementById("navbar-secondary").style.height = "120px";
+      document.getElementById("navbar-secondary").style.boxShadow = "none";
+      document.getElementById("navbar-title").style.fontSize = "28px"
+      document.querySelector(".navbar-title").style.lineHeight = "38px"
+      document.getElementById("home-link").style.color = "white"
+      document.getElementById("service-link").style.color = "white"
+      document.getElementById("about-link").style.color = "white"
+      document.getElementById("contact-link").style.color = "white"
 
-    document.getElementById("navbar-secondary").style.background = "transparent";
+      document.getElementById("navbar-secondary").style.background = "transparent";
 
 
 
-    // document.getElementById("logo").style.fontSize = "35px";
+      // document.getElementById("logo").style.fontSize = "35px";
+    } else {
+      document.getElementById("navbar-secondary").style.background = "transparent";
+      document.getElementById("navbar-secondary").style.boxShadow = "none";
+      
+    }
   }
+    
 }
